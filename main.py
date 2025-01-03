fb5650a9b697cb8da479f0e003d2b46844fe8bc2
Ryan Sands <ryan.sands@icloud.com>
2025-01-03 22:58:13 +0000

    Refactor game loop to utilize sprite groups for updating and drawing objects

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from player import Player
 
 
 def gameloop():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -13,10 +17,15 @@ def gameloop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
-            
-        player.update(dt)
+        
         screen.fill((0, 0, 0))
-        player.draw(screen)
+            
+        for object in updatable:
+            object.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
         clock_delta_time = clock.tick(60)
         dt = clock_delta_time / 1000
@@ -25,8 +34,6 @@ def main() -> int:
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-
-    
 
     pygame.init()
 
